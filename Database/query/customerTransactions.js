f8ad6df81e39e50f4c92dddec84356f98d0e0ac7
Yusuf Dede <yusufdede95@hotmail.com>
2019-11-03 04:05:38 +0300

    customerTransaction update

--- Database/query/customerTransactions.js
+++ Database/query/customerTransactions.js
@@ -50,8 +50,9 @@ module.exports.LoginUser = async (data) => {
 module.exports.UpdateUser = async (data) => {
     const pool = await sql.getConnection();
     try {
-        let result1 = await pool.request()
+        let result = await pool.request()
             .input('tc', mssql.BigInt, data.tc)
+            .input('pw', mssql,NVarChar, data.pw)
             .input('firstName', mssql.VarChar, data.firstName)
             .input('lastName', mssql.VarChar, data.lastName)
             .input('birthDate', mssql.Date, data.date)
